fd766e41f56843d02c7d8e78612accf6f718b77b
2019tianxinyua <tianxinyua@126.com>
2020-02-20 17:53:08 +0800

    第一次作业

---
+++ Homework01/calculator01/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("请输入一个整数：");
+            string num1 = Console.ReadLine();
+            int number1 = CheckNum(num1);
+            Console.WriteLine("请输入第二个整数：");
+            string num2 = Console.ReadLine();
+            int number2 = CheckNum(num2);
+            //选择运算符
+            Console.WriteLine("请选择运算符：1.+  2.-  3.x  4.÷ ");
+            string fun = Console.ReadLine();
+            GetResualt(fun, number1, number2);
+            Console.ReadLine();
+        }
+
+//错误处理，检测是否是整数，不是的话打印提示
+        static int CheckNum(string num)
+        {
+            try
+            {
+                int i = int.Parse(num);
+                return i;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("输入有误，请重新输入：");
+                string str = Console.ReadLine();
+                //递归算法
+                return CheckNum(str);
+            }
+        }
+        //进行运算
+        static void GetResualt(string fun, int num1, int num2)
+        {
+            int res = 0;
+            string yun = "";
+            switch (fun)
+            {
+                case "1":
+                    res = num1 + num2;
+                    yun = "+";
+                    break;
+                case "2":
+                    res = num1 - num2;
+                    yun = "-";
+                    break;
+                case "3":
+                    res = num1 * num2;
+                    yun = "x";
+                    break;
+                case "4":
+                    res = num1 / num2;
+                    yun = "÷";
+                    break;
+                default:
+                    Console.WriteLine("请重新选择运算符：");
+                    string str = Console.ReadLine();
+                    GetResualt(str, num1, num2);
+                    return;
+            }
+            Console.WriteLine("{0}{1}{2}={3}", num1, yun, num2, res);
+        }
+    }
+}
---
+++ Homework01/calculatorwinform/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculatorwinform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//+
+
+            string strNum1 = textBox1.Text;
+            if (string.IsNullOrEmpty(strNum1))
+            {
+                strNum1 = "0";
+                MessageBox.Show("请检查并重新输入正确的值！", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            }
+            string strNum2 = textBox2.Text;
+            if (string.IsNullOrEmpty(strNum2))
+            {
+                strNum2 = "0";
+                MessageBox.Show("请检查并重新输入正确的值！", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            }
+            float flNum1 = Convert.ToSingle(strNum1);
+            float flNum2 = Convert.ToSingle(strNum2);
+            float result = flNum1 + flNum2;
+            textBox3.Text = Convert.ToString(result);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//减
+            string strNum1 = textBox1.Text;
+            if (string.IsNullOrEmpty(strNum1))
+            {
+                strNum1 = "0";
+                MessageBox.Show("请检查并重新输入正确的值！", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            }
+            string strNum2 = textBox2.Text;
+            if (string.IsNullOrEmpty(strNum2))
+            {
+                strNum2 = "0";
+                MessageBox.Show("请检查并重新输入正确的值！", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            }
+            float flNum1 = Convert.ToSingle(strNum1);
+            float flNum2 = Convert.ToSingle(strNum2);
+            float result = flNum1 - flNum2;
+            textBox3.Text = Convert.ToString(result);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {//乘法
+            string strNum1 = textBox1.Text;
+            if (string.IsNullOrEmpty(strNum1))
+            {
+                strNum1 = "0";
+                MessageBox.Show("请检查并重新输入正确的值！", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            }
+            string strNum2 = textBox2.Text;
+            if (string.IsNullOrEmpty(strNum2))
+            {
+                strNum2 = "0";
+                MessageBox.Show("请检查并重新输入正确的值！", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            }
+            float flNum1 = Convert.ToSingle(strNum1);
+            float flNum2 = Convert.ToSingle(strNum2);
+            float result = flNum1 * flNum2;
+            textBox3.Text = Convert.ToString(result);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            {//除法
+                string strNum1 = textBox1.Text;
+                if (string.IsNullOrEmpty(strNum1))
+                {
+                    strNum1 = "0";
+                    MessageBox.Show("请检查并重新输入正确的值！", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                }
+                string strNum2 = textBox2.Text;
+                if (string.IsNullOrEmpty(strNum2))
+                {
+                    strNum2 = "0";
+                    MessageBox.Show("请检查并重新输入正确的值！", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                }
+                if (int.Parse(strNum2) == 0)
+                {
+                    MessageBox.Show("请检查并重新输入正确的值！", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                }
+                float flNum1 = Convert.ToSingle(strNum1);
+                float flNum2 = Convert.ToSingle(strNum2);
+                float result = flNum1 / flNum2;
+                textBox3.Text = Convert.ToString(result);
+            }
+
+        }
+    }
+}

058f6aaed013fbd737b6a66989c55ef4d435eaa2
2019tianxinyua <tianxinyua@126.com>
2020-03-08 13:10:55 +0800

    第三周练习

---
+++ Homework03/shape01.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape01
+{
+    public abstract class shape01
+    {
+        public abstract double area();//计算面积的抽象方法
+    }
+
+    interface Reasonable
+    {
+        bool Reasonable();
+    }
+    class rectangle : shape01,Reasonable//长方形类
+    {
+       
+        public double length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                length = value;
+            }
+
+        }
+        public double width
+        {
+            get
+            {
+                return width;
+            }
+             set
+            {
+                length = value;
+            }
+
+        }
+        public rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+
+        public override double area()
+        {
+            return length * width;
+        }
+        public bool  Reasonable(double length,double width)
+        {
+            if (length > 0 && width > 0 && length > width)
+     
+                return true;
+         
+            else
+                return false;
+
+
+         }
+        
+     
+    }    
+    class square : shape01,Reasonable//正方形类
+    {
+        private double sidelength;
+        public double sidelength
+        {
+            get;
+            set;
+        }
+        public square(double sidelength)
+        {
+            this.sidelength = sidelength;
+        }
+        public override double area()
+        {
+            return sidelength * sidelength;
+        }
+        public bool Reasonable(double sidelength)
+        {
+            if (sidelength > 0)
+                return true;
+  
+            else
+                return false;
+ 
+        }
+  
+    }
+
+    class triangle : shape01,Reasonable//三角形类
+    {
+        private double length1;
+        private double length2;
+        private double length3;
+        public double length1
+        {
+            get;
+            set;
+        }
+        public double length2
+        {
+            get;
+            set;
+        }
+        public double length3
+        {
+            get;
+            set;
+        }
+        public triangle(double length1,double length2,double length3)
+        {
+            this.length1 = length1;
+            this.length2 = length2;
+            this.length3 = length3;
+
+        }
+        public override double area()
+        {
+            double sum = (length1 + length2 + length3) / 2;//海伦公式
+            return Math.Sqrt(sum*(sum-length1)*(sum-length2)*(sum-length3));
+        }
+        public bool Reasonable(double length1,double length2,double length3)
+        {
+            bool a = length1 > 0 && length2 > 0 && length3 > 0 && (length1 + length2 > length3) && (length2 + length3 > length1) && (length1 + length3 > length2);
+            if (a == true)
+            {
+                return true;
+                if(length1 == length2 && length2 == length3)
+                {
+                    Console.WriteLine("是等边三角形");
+                }
+                else if (length1 == length2 || length2 == length3 || length3 == length1)
+                    Console.WriteLine("是等腰三角形");
+            }
+            else
+                return false;
+
+        }
+
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Homework03/shape02.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape02
+{
+    public interface Shape02
+    {
+        string creat();
+    }
+    public class ShapeCreat : Shape02
+    {
+        public double getrandomshape()
+        {
+            string[] randomshape = new string[10];
+            for (int i = 0; i < num; i++)
+            {
+                Random random = new Random();
+                int j = random.Next(0, 3);//产生位于0~3间的整数
+                randomshape[i] = creat(j);
+            }
+        }
+        public double creat(int j)
+        {
+            switch (j)
+            {
+                case 0:
+                    rectangle rt = new rectangle(height, width);
+                    return rt.Area();
+                    break;
+                case 1:
+                    square s = new square(width);
+                    return s.Area();
+                    break;
+                case 2:
+                    triangle tr = new triangle(l1, l2, l3);
+                    return tr.Area();
+                    break;
+                default:
+                    return null;
+                    break;
+
+            }
+        }
+
+        abstract class shape
+        {
+            public abstract double Area();//面积
+            public abstract string Initialization();
+        }
+        class rectangle : shape
+        {
+            double width;
+            double height;
+            public override string Initialization()
+            {
+                return new Random(Guid.NewGuid().GetHashCode()).Next(width, height);
+
+            }
+            public override double Area(double w, double h)
+            {
+                Random random = new Random();
+                double w = random.Next(0, 100);
+                double h = random.Next(0, 100);
+                this.width = w;
+                this.height = h;
+                return width * height;
+            }
+
+        }
+        class square : shape
+        {
+            double width;
+            public override double Area(double w)
+            {
+                Random random = new Random();
+                double w = random.Next(0, 100);
+                this.width = w;
+                return width * width;
+            }
+            public override string Initialization()
+            {
+                return new Random(Guid.NewGuid().GetHashCode()).Next(width);
+
+            }
+
+        }
+        class triangle : shape
+        {
+            double l1;
+            double l2;
+            double l3;
+            public override double Area(double a, double b, double c)
+            {
+                Random random = new Random();
+                double a = random.Next(0, 100);
+                double b = random.Next(0, 100);
+                double c = random.Next(0, 100);
+                this.l1 = a;
+                this.l2 = b;
+                this.l3 = c;
+                double p = (l1 + l2 + l3) / 2;
+                return Math.Sqrt(p * (p - l1) * (p - l2) * (p - l3));
+            }
+            public override string Initialization()
+            {
+                return new Random(Guid.NewGuid().GetHashCode()).Next(l1, l2, l3);
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int sum = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    sum += randomshape[i].Area();
+                }
+            }
+        }
+    }
+}
+

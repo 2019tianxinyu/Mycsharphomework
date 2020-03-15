48e36472f80ac29a5f3a15d4d293e1f0943d80d9
2019tianxinyua <tianxinyua@126.com>
2020-03-15 09:28:31 +0800

    Homework04

---
+++ Homework04/Clock.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace clock
+{//建立委托
+    public delegate void TickHandle(object sender, Tick args);
+    public delegate void AlarmHandle(object sender, Alarm args);
+    public class Tick
+    {
+        public Tick() { }//创建滴答
+    }
+    public class Alarm
+    {
+        public Alarm() { }//创建响铃
+    }
+    public class AlarmClock
+    {
+        public event TickHandle ATick;
+        public event AlarmHandle AAlarm;
+        public int count;//响铃间隔
+        public void setAlarm(int a)
+        {
+            this.count = a;
+        }
+        //启动闹钟
+        public void start()
+        {
+            for (int i = 1; ; i++)
+            {
+                Thread.Sleep(1000);
+                ClockTick();
+                if (i % count == 0)
+                {
+                    ClockAlarm();
+                }
+            }
+        }
+          public void ClockAlarm()
+             {
+                 Alarm args = new Alarm();
+                AAlarm(this, args);
+              }  
+          public void ClockTick()
+             {
+                 Tick args = new Tick();
+                 ATick(this, args);
+             }
+    }
+ 
+
+    public  class Manager
+{
+    public AlarmClock Clock1 = new AlarmClock();
+    public Manager()
+    {
+        Clock1.ATick += new TickHandle(Clock_Tick);
+        Clock1.AAlarm += new AlarmHandle(Clock_Alarm);
+
+    }
+    void Clock_Tick(object sender,Tick args)
+    {
+        Console.WriteLine("滴答");
+
+    }
+    void Clock_Alarm(object sender,Alarm args)
+    {
+        Console.WriteLine("响铃");
+    }
+}
+     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Manager demo = new Manager();
+        demo.Clock1.setAlarm(60);
+        demo.Clock1.start();
+        }
+    }
+}
---
+++ Homework04/GenericApplication.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication {
+
+  // 链表节点
+  public class Node<T> {
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+
+    public Node(T t) {
+      Next = null;
+      Data = t;
+    }
+  }
+
+  //泛型链表类
+  public class GenericList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+
+    public GenericList() {
+      tail = head = null;
+    }
+
+    public Node<T> Head {
+      get => head;
+    }
+
+    public void Add(T t) {
+      Node<T> n = new Node<T>(t);
+      if (tail == null) {
+        head = tail = n;
+      }else {
+        tail.Next = n;
+        tail = n;
+      }
+    }
+        public void foreach01(Action<T> action1)
+        {
+            Node<T> a;
+            a = this.head;
+            while (a!= tail)
+            {
+                action1(a.Data);
+                a = a.Next;
+            }
+        }
+
+  class Program {
+    static void Main(string[] args) {
+                int sum = 0;
+                int max = 0;
+                int min = int.MaxValue;
+                GenericList<int> intList = new GenericList<int>();
+                for (int x = 0; x < 10; x++)
+                {
+                    intList.Add(x);
+                }
+                intList.foreach01(s => Console.WriteLine(s));
+                int max = int.MinValue;
+                intList.foreach01(n => { max = max < n ? n : max; });
+                Console.WriteLine($"最大值：{max}");
+                int min = int.MaxValue;
+                intList.foreach01(n => { min = min > n ? n : min; });
+                Console.WriteLine($"最小值：{min}");
+                intList.foreach01(n => { sum += n; });
+                Console.WriteLine($"和：{sum}");
+            }
+     
+
+    }
+
+  }
+}

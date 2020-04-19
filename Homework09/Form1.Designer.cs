d80893e82b546bbbdbe71f5c3e4cc00888a7c6f8
2019tianxinyua <tianxinyua@126.com>
2020-04-19 09:42:39 +0800

    Homework09

---
+++ Homework09/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Crawler2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(574, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "点击开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "网址";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(452, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "内容";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(92, 154);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(452, 274);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Homework09/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crawler2
+{
+   
+    public partial class Form1 : Form
+    {
+        Crawler2 crawler = new Crawler2();
+        public Form1()
+        {
+            InitializeComponent();
+            crawler.PageDownload += Crawler_PageDownload;
+        }
+
+        private void Crawler_PageDownload(string obj)
+        {
+            if (this.listBox1.InvokeRequired)
+            {
+                Action<String> action = this.AddUrl;
+                this.Invoke(action, new object[] { obj });
+            }
+            else
+            {
+                listBox1.Items.Add(obj);
+            }
+        }
+
+        private void AddUrl(string url)
+        {
+            listBox1.Items.Add(url);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            crawler.StartUrl = textBox1.Text;
+            listBox1.Items.Clear();
+            new Thread(crawler.Start).Start();
+
+        }
+    }
+    class Crawler2
+    {
+        public event Action<String> PageDownload;
+        private Hashtable urls = new Hashtable();
+        private int n = 0;
+        private string startUrl = "";
+        public string StartUrl
+        {
+            get => startUrl;
+            set
+            {
+                startUrl = value;
+                urls = new Hashtable();
+                urls.Add(value, false);
+            }
+
+        }
+
+        public  void Start(){
+            Console.WriteLine("----开始爬行----");
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url])
+                        continue;
+                    current = url;
+                }
+                if (current == null || n > 10) break;
+                Console.WriteLine("爬行" + current + "页面");
+                string html = Download(current);
+                urls[current] = true;
+                n++;
+                Parse(html);
+                Console.WriteLine("----爬行结束----");
+            }
+        }
+
+        private string Download(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = n.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                PageDownload(url);
+                return html;
+            }
+            catch(Exception ex)
+            {
+                PageDownload(url + " Error:" + ex.Message);
+                return "";
+
+
+            }
+        }
+        private void Parse(string html)
+        {
+            string strRef = @"(href | HREF)[] *=[] *[""'][^""'#>]+[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)
+                          .Trim('"', '\"', '#', '>');
+                if (strRef.Length == 0) continue;
+                if (urls[strRef] == null) urls[strRef] = false;
+            }
+        }
+
+    }
+}
